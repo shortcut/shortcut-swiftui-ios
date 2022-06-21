2aafff9cbce470cab8f2020fc8657c53e9c4e09d
Swathi Kumar <swathi.kumar@shortcut.no>
2022-06-21 12:48:03 +0200

    Added helpers/ extensions for NavigationLink and View onLoad

---
+++ Sources/ShortcutUI/SwiftUI/View+OnLoad.swift
@@ -0,0 +1,38 @@
+//
+//  ViewDidLoadModifier.swift
+//  ShortcutUI
+//
+//  Created by Swathi on 2022-06-09.
+//  Copyright © 2021 Shortcut Scandinavia Apps AB. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct ViewDidLoadModifier: ViewModifier {
+
+    @State private var didLoad = false
+    private let action: (() -> Void)?
+
+    init(perform action: (() -> Void)? = nil) {
+        self.action = action
+    }
+
+    func body(content: Content) -> some View {
+        content.onAppear {
+            if didLoad == false {
+                didLoad = true
+                action?()
+            }
+        }
+    }
+
+}
+
+extension View {
+
+    public func onLoad(perform action: (() -> Void)? = nil) -> some View {
+        modifier(ViewDidLoadModifier(perform: action))
+    }
+
+}
